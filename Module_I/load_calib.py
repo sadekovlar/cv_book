dde42da14c90b21073c875e040b1ae1b46d90e1a
Садеков Ринат <r.sadekov@cpilot.cognitivepilot.com>
2022-03-07 20:11:06 +0300

    Добавил новые видеоролики ко второму практическому заданию

--- Module_I/load_calib.py
+++ Module_I/load_calib.py
@@ -13,8 +13,8 @@ class CalibReader:
         file_name = self._file_name
         list_param = self._param
         fs = cv2.FileStorage(file_name, cv2.FILE_STORAGE_READ)
+        param = dict()
         if fs.isOpened():
-            param = dict()
             for index in list_param:
                 param[index] = fs.getNode(index).mat()
         fs.release()
--- example.py
+++ example.py
@@ -1,23 +1,50 @@
 import cv2
+import math as mt
+import numpy as np
 from season_reader import SeasonReader
+from Module_I.load_calib import CalibReader
 
 class MyReader(SeasonReader):
     def on_init(self):
+        par = ["K", "D", "r", "t" ]
+        calib = CalibReader()
+        calib.initialize(file_name = './data/tram/leftImage.yml', param = par)
+        self.matrix = calib.read()
         return True
 
     def on_shot(self):
         return True
 
     def on_frame(self):
+        Xw =  np.array([15, 0, 0])
+        r = self.matrix["r"].reshape(-1)
+        t = self.matrix["t"].reshape(3, 1)
+        K = self.matrix["K"]
+        R = self.get_R(r)
+        Xc = R.inv() @ Xw + t.T
+        u = Xc[0,0]/Xc[0,1]*K[0,0] + K[0,2]
+        v = Xc[0,2]/Xc[0,1]*K[1,1] + K[1,2]
         cv2.putText(self.frame, f"GrabMsec: {self.frame_grab_msec}", (15, 50),
                 cv2.FONT_HERSHEY_PLAIN, 1.0, (0, 255, 0), 2)
         return True
+
+    def get_R(self, r):
+        cos_x = mt.cos(r[0])
+        sin_x = mt.sin(r[0])
+        cos_y = mt.cos(r[1])
+        sin_y = mt.sin(r[1])
+        cos_z = mt.cos(r[2])
+        sin_z = mt.sin(r[2])
+        R_x = np.array([[1, 0, 0],          [0, cos_x, -sin_x], [0, sin_x,  cos_x]])
+        R_y = np.array([[cos_y, 0, sin_y],  [0, 1, 0],          [-sin_y, 0, cos_y]])
+        R_z = np.array([[cos_z, -sin_z, 0], [sin_z, cos_z, 0],  [0, 0,  1]])
+        return R_z * R_y * R_x
+
 
     def on_gps_frame(self):
         shot: dict = self.shot[self._gps_name]['senseData']
         shot['grabMsec'] = self.shot[self._gps_name]['grabMsec']
         return True
-
         
     def on_imu_frame(self):
         shot: dict = self.shot[self._imu_name]
