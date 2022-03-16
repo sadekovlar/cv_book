023b8261da8dc2e27909e5c51e49c4e32d3858d0
r.sadekov <r.sadekov@cognitivepilot.com>
2022-03-16 23:12:23 +0300

    Переместил файл season stream ниже в папку

--- Module_I/task_1.py
+++ Module_I/task_1.py
@@ -1,9 +1,11 @@
 import cv2
 import numpy as np
+from load_calib import CalibReader
+from season_reader import SeasonReader
+from spatial_geometry_tools.calib import Calib
+from spatial_geometry_tools.camera import Camera
+from spatial_geometry_tools.point import Point3d as Point
 
-from Module_I.spatial_geometry_tools.calib import Calib
-from Module_I.spatial_geometry_tools.camera import Camera
-from Module_I.spatial_geometry_tools.point import Point3d as Point
 
 BLACK = (0, 0, 0)
 BLUE = (255, 0, 0)
@@ -27,34 +29,68 @@ class WayEstimator:
         left_2d_far = self.camera.project_point_3d_to_2d(self.left_3d_far)
         right_2d_near = self.camera.project_point_3d_to_2d(self.right_3d_near)
         right_2d_far = self.camera.project_point_3d_to_2d(self.right_3d_far)
-
         cv2.line(img, right_2d_near, right_2d_far, BLACK, LINE_WIDTH)
         cv2.line(img, left_2d_near, left_2d_far, BLACK, LINE_WIDTH)
-
         return img
 
     def draw_coordinate_system(self, img):
         center3d = Point((0, 0, 0))
         center2d = self.camera.project_point_3d_to_2d(center3d)
         cv2.circle(img, center2d, 5, BLACK, 5)
-
         end = 10
         for i in range(1, end):
             x3d = Point((i, 0, 0))
             y3d = Point((0, i, 0))
             z3d = Point((0, 0, i))
-
             x2d = self.camera.project_point_3d_to_2d(x3d)
             y2d = self.camera.project_point_3d_to_2d(y3d)
             z2d = self.camera.project_point_3d_to_2d(z3d)
-
             cv2.circle(img, x2d, 5, BLUE, 5)
             cv2.circle(img, y2d, 5, GREEN, 5)
             cv2.circle(img, z2d, 5, RED, 5)
-
             if i == end-1:
                 cv2.line(img, x2d, center2d, BLUE, LINE_WIDTH)
                 cv2.line(img, y2d, center2d, GREEN, LINE_WIDTH)
                 cv2.line(img, z2d, center2d, RED, LINE_WIDTH)
-
         return img
+
+
+class Reader(SeasonReader):
+    """Обработка видеопотока."""
+    def on_init(self, _file_name: str = None):
+        par = ['K', 'D', 'r', 't']
+        calib_reader = CalibReader()
+        calib_reader.initialize(
+            file_name='../data/tram/leftImage.yml',
+            param=par)
+        calib_dict = calib_reader.read()
+        self.way_estimator = WayEstimator(calib_dict, 10)
+        return True
+
+    def on_shot(self):
+        return True
+
+    def on_frame(self):
+        cv2.putText(self.frame, f'GrabMsec: {self.frame_grab_msec}', (15, 50),
+                    cv2.FONT_HERSHEY_PLAIN, 1.0, (0, 255, 0), 2)
+        self.way_estimator.dray_way(self.frame)
+        return True
+
+    def on_gps_frame(self):
+        shot: dict = self.shot[self._gps_name]['senseData']
+        shot['grabMsec'] = self.shot[self._gps_name]['grabMsec']
+        return True
+
+    def on_imu_frame(self):
+        shot: dict = self.shot[self._imu_name]
+        return True
+
+
+if __name__ == '__main__':
+    init_args = {
+        'path_to_data_root': '../data/tram/'
+    }
+    s = Reader()
+    s.initialize(**init_args)
+    s.run()
+    print('Done!')
