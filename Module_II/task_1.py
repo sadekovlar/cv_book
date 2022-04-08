2a7f60443c3624d85e42df423a78c97bf148dfa4
AlexaEgorova <egorova.a007@gmail.com>
2022-04-08 16:44:14 +0300

    add overexposure localization

--- Module_II/task_1.py
+++ Module_II/task_1.py
@@ -1,19 +1,44 @@
 import cv2
 import numpy as np
 
-class CarLightsStruckReducer:
-    '''Класс для уменьшения засветки от фар встречного ТС'''
+
+class LightsStruckReducer:
+    """Класс для приглушения засветки от фар"""
+
     @staticmethod
-    def __gamma_correction(img: np.ndarray, gamma: float) -> np.ndarray:
-        look_up_table = np.empty((1,256), np.uint8)
+    def gamma_correction(img: np.ndarray, gamma: float) -> np.ndarray:
+        look_up_table = np.empty((1, 256), np.uint8)
         for i in range(256):
-            look_up_table[0,i] = np.clip(pow(i / 255.0, gamma) * 255.0, 0, 255)
+            look_up_table[0, i] = np.clip(pow(i / 255.0, gamma) * 255.0, 0, 255)
         res = cv2.LUT(img, look_up_table)
         return res
 
     @staticmethod
-    def reduce_light_struck(img: np.ndarray) -> np.ndarray:
-        res = CarLightsStruckReducer.__gamma_correction(img, 1.4)
+    def get_map_mask(img):
+        """Получение маски на основе карты расстояний до источника засветки"""
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        overexposed_mask = cv2.threshold(gray, 220, 255, cv2.THRESH_BINARY)[1]
+        overexposed_mask_inv = cv2.bitwise_not(overexposed_mask)
+        dist = cv2.distanceTransform(overexposed_mask_inv,
+                                     cv2.DIST_L2,
+                                     cv2.DIST_MASK_3,
+                                     overexposed_mask_inv)
+        cv2.normalize(dist, dist, 0.0, 20.0, cv2.NORM_MINMAX)
+        map_mask_inv = np.zeros(dist.shape, dtype=np.uint8)
+        map_mask_inv[np.where(dist > 1)] = 255
+        map_mask = np.zeros(dist.shape, dtype=np.uint8)
+        map_mask[np.where(map_mask_inv == 0)] = 255
+        return map_mask
+
+    @staticmethod
+    def reduce_lights_struck(img: np.ndarray) -> np.ndarray:
+        res = img.copy()
+        overexposed_mask = LightsStruckReducer.get_map_mask(res)
+        mask_inv = cv2.bitwise_not(overexposed_mask)
+        non_overexposed_part = cv2.bitwise_and(res, res, mask=mask_inv)
+        overexposed_part = cv2.bitwise_and(res, res, mask=overexposed_mask)
+        corrected = LightsStruckReducer.gamma_correction(overexposed_part, 1.06)
+        res = cv2.add(non_overexposed_part, corrected)
         return res
 
 
@@ -23,9 +48,9 @@ if __name__ == '__main__':
     while cap.isOpened():
         succeed, frame = cap.read()
         if succeed:
-            frame = CarLightsStruckReducer.reduce_light_struck(frame)
+            frame = LightsStruckReducer.reduce_lights_struck(frame)
             cv2.imshow(video_name, frame)
         else:
             cv2.destroyAllWindows()
             cap.release()
-        cv2.waitKey(10)
+        cv2.waitKey(1)
