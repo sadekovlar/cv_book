2cd04aa9273641bc6d1effca8cb723e695d00f3d
pibragimov <pibragimov@whoosh.bike>
2023-02-13 11:56:20 +0300

    ref: black and remove unused imports

--- Module_1/task1.py
+++ Module_1/task1.py
@@ -1,15 +1,15 @@
 import cv2
-import numpy as np 
-input = cv2.imread('image.jpg')
-cv2.imshow('Hello World', input)
+
+input_ = cv2.imread("image.jpg")
+cv2.imshow("Hello World", input_)
 cv2.waitKey()
 cv2.destroyAllWindows()
 
-print(input.shape)
+print(input_.shape)
 
-print('Height of Image:', int(input.shape[0]), 'pixels')
-print('Width of Image: ', int(input.shape[1]), 'pixels')
-print('No of RGB elements: ', int(input.shape[2]), 'values')
+print("Height of Image:", int(input_.shape[0]), "pixels")
+print("Width of Image: ", int(input_.shape[1]), "pixels")
+print("No of RGB elements: ", int(input_.shape[2]), "values")
 
-cv2.imwrite('output.jpg', input)
-cv2.imwrite('output.png', input)
+cv2.imwrite("output.jpg", input_)
+cv2.imwrite("output.png", input_)
