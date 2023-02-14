0e33bd255c913ec318a7b7ea709ab17882871dc8
nfrmtk <nfrmtk.aka.fkfka@gmail.com>
2023-02-14 22:54:35 +0300

    add image + add play with matrices

--- Module_1/task6.py
+++ Module_1/task6.py
@@ -9,10 +9,24 @@ depth  = image.shape[2]
 M = np.ones([height,width, depth], dtype="uint8")*100
 
 added = cv2.add(image, M)
-
-cv2.imshow('added', added)
+print(image[0,0])
+cv2.imshow('added', image)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
 
 subtracted = cv2.subtract(M, image)
-M = np.ones(image.shape, dtype = "uint8") * 75 
+M = np.ones(image.shape, dtype = "uint8") * 100
+red = np.dot(M, [
+    [0, 0, 1],
+    [0, 1, 0],
+    [1, 0, 0],
+])
+some = image.dot(    
+    np.array([[0, 0, 1],
+    [1, 0, 0],
+    [0, 1, 0]], dtype="uint8")
+)
+
+cv2.imshow('weird', some)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
