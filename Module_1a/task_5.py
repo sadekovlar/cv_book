55ebada323f81a029604dfc6a910fe1b958971fd
Nikolay Nyunin <nikolay.nyunin@gmail.com>
2023-02-24 02:01:11 +0300

    Convert functions into methods in Module_1a/task_5.py

--- Module_1a/task_5.py
+++ Module_1a/task_5.py
@@ -13,84 +13,24 @@ import numpy as np
 
 from srccam import CalibReader, Calib, Camera, SeasonReader, Point3d as Point
 
-# пути к архивам с данными
-DATA_PATHS = ['../data/city/trm.169.007.info.yml.gz',
-              '../data/city/trm.169.008.info.yml.gz']
-
-GPS_TAG = 'emlidLeft'
-
-RED = (0, 0, 255)
-
-
-def get_gps_data(file_paths: list) -> list:
-    """Получение информации о пути трамвая из архивов с данными."""
-
-    data = []
-    for file_path in file_paths:
-        if not os.path.isfile(file_path):
-            raise FileNotFoundError('File {} not found.'.format(file_path))
-
-        with gzip.open(file_path, 'rt') as file:
-            file.readline()
-            file = file.read()
-            file = file.replace(':', ': ')
-            yml = yaml.load(file, Loader=Loader)
-            for shot in yml['shots']:
-                if GPS_TAG in shot:
-                    sense_data = shot[GPS_TAG]['senseData']
-                    data.append({'nord': sense_data['nord'],
-                                 'east': sense_data['east']})
-
-    return data
-
-
-def get_dist_and_angle(lat1: float, long1: float, lat2: float, long2: float) -> (float, float):
-    """Вычисление расстояния между точками и начального азимута по их координатам."""
-
-    # радиус Земли
-    rad = 6372795
-
-    # перевод координат в радианы
-    lat1 = lat1 * math.pi / 180
-    lat2 = lat2 * math.pi / 180
-    long1 = long1 * math.pi / 180
-    long2 = long2 * math.pi / 180
-
-    # косинусы и синусы широт и разницы долгот
-    cl1 = math.cos(lat1)
-    cl2 = math.cos(lat2)
-    sl1 = math.sin(lat1)
-    sl2 = math.sin(lat2)
-    delta = long2 - long1
-    c_delta = math.cos(delta)
-    s_delta = math.sin(delta)
-
-    # вычисление расстояния между точками
-    y = math.sqrt(math.pow(cl2 * s_delta, 2) + math.pow(cl1 * sl2 - sl1 * cl2 * c_delta, 2))
-    x = sl1 * sl2 + cl1 * cl2 * c_delta
-    ad = math.atan2(y, x)
-    dist = ad * rad
-
-    # вычисление начального азимута
-    y = s_delta * cl2
-    x = (cl1 * sl2) - (sl1 * cl2 * c_delta)
-    angle_rad = math.atan2(y, x)
-
-    return dist, angle_rad
-
 
 class PathCreator:
     """Вычисление и отображение пути следования трамвая из данных GPS."""
 
-    POINT_CNT = 50    # количество точек для отрисовки
-    COLOR = RED       # цвет линий
-    LINE_WIDTH = 5    # толщина линий
+    # пути к архивам с данными
+    DATA_PATHS = ['../data/city/trm.169.007.info.yml.gz',
+                  '../data/city/trm.169.008.info.yml.gz']
+
+    GPS_TAG = 'emlidLeft'  # ключ для получения данных GPS
+    POINT_CNT = 50         # количество точек для отрисовки
+    COLOR = (0, 0, 255)    # цвет линий (красный)
+    LINE_WIDTH = 5         # толщина линий
 
     def __init__(self, calib) -> None:
         self.camera = Camera(calib)
 
         # данные GPS
-        self.data = get_gps_data(DATA_PATHS)
+        self.data = self.get_gps_data()
 
         # расстояния и азимуты для соседних точек пути
         self.dists, self.angles = np.zeros(len(self.data) - 1), np.zeros(len(self.data) - 1)
@@ -110,12 +50,68 @@ class PathCreator:
         # точки в 2D-пространстве для отрисовки пути
         self.points_2d = None
 
+    def get_gps_data(self) -> list:
+        """Получение информации о пути трамвая из архивов с данными."""
+
+        data = []
+        for file_path in self.DATA_PATHS:
+            if not os.path.isfile(file_path):
+                raise FileNotFoundError('File {} not found.'.format(file_path))
+
+            with gzip.open(file_path, 'rt') as file:
+                file.readline()
+                file = file.read()
+                file = file.replace(':', ': ')
+                yml = yaml.load(file, Loader=Loader)
+                for shot in yml['shots']:
+                    if self.GPS_TAG in shot:
+                        sense_data = shot[self.GPS_TAG]['senseData']
+                        data.append({'nord': sense_data['nord'],
+                                     'east': sense_data['east']})
+
+        return data
+
     def process_data(self) -> None:
-        """Нахождение расстояний и углов для всех известных точек."""
+        """Нахождение расстояний и азимутов для всех известных точек."""
 
         for i in range(len(self.data) - 1):
             p1, p2 = self.data[i], self.data[i + 1]
-            self.dists[i], self.angles[i] = get_dist_and_angle(p1['nord'], p1['east'], p2['nord'], p2['east'])
+            self.dists[i], self.angles[i] = self.get_dist_and_angle(p1['nord'], p1['east'], p2['nord'], p2['east'])
+
+    @staticmethod
+    def get_dist_and_angle(lat1: float, long1: float, lat2: float, long2: float) -> (float, float):
+        """Вычисление расстояния и начального азимута между двумя точками по их координатам."""
+
+        # радиус Земли
+        rad = 6372795
+
+        # перевод координат в радианы
+        lat1 = lat1 * math.pi / 180
+        lat2 = lat2 * math.pi / 180
+        long1 = long1 * math.pi / 180
+        long2 = long2 * math.pi / 180
+
+        # косинусы и синусы широт и разницы долгот
+        cl1 = math.cos(lat1)
+        cl2 = math.cos(lat2)
+        sl1 = math.sin(lat1)
+        sl2 = math.sin(lat2)
+        delta = long2 - long1
+        c_delta = math.cos(delta)
+        s_delta = math.sin(delta)
+
+        # вычисление расстояния между точками
+        y = math.sqrt(math.pow(cl2 * s_delta, 2) + math.pow(cl1 * sl2 - sl1 * cl2 * c_delta, 2))
+        x = sl1 * sl2 + cl1 * cl2 * c_delta
+        ad = math.atan2(y, x)
+        dist = ad * rad
+
+        # вычисление начального азимута
+        y = s_delta * cl2
+        x = (cl1 * sl2) - (sl1 * cl2 * c_delta)
+        angle_rad = math.atan2(y, x)
+
+        return dist, angle_rad
 
     def next_point(self) -> None:
         """Добавление следующей точки в маршрут."""
