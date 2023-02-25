e108a17a36763a9246b7ad4cb6475f6c4f0a4654
oCatano <sanya11112002@mail.ru>
2023-02-25 10:05:21 +0300

    add functions to augmentations

--- Module_1/augmentations.py
+++ Module_1/augmentations.py
@@ -16,22 +16,20 @@ def augmentation(paths: List[str], output_directory: str) -> None:
         img = cv2.imread(path)
         cv2.imwrite(output_directory + '/' + file_name, img)
 
+        # Разделение на каналы
+        (b, g, r) = cv2.split(img)
+
         # Добавление картинок с разными  цветами
         for i in range(3):
-            for coef in range(1, 12, 3):
-                img_first_dim_change = img.copy()
-                img_first_dim_change[:, :, 0] = coef * img_first_dim_change[:, :, 0]
-                for coef_2 in range(1, 12, 3):
-                    img_second_dim_change = img_first_dim_change.copy()
-                    img_second_dim_change[:, :, (i + 1) % 3] *= coef_2 * img_second_dim_change[:, :, (i + 1) % 3]
-                    cv2.imwrite(output_directory + '/' + file_name + str(coef) + str(coef_2) + str(i) + '.jpg',
-                                img_second_dim_change)
+            cv2.imwrite(output_directory + '/' + file_name + str(i) + '.jpg', cv2.merge((b * i, g * i * 2, r)))
+
+        # применил transpose к фото
+        cv2.imwrite(output_directory + '/' + file_name + 'transpose' + '.jpg', img.transpose().reshape(2048, 2048, 3))
 
         # Повысил резкость
         kernel = np.array([[-1, -1, -1], [-1, 15, -1], [-1, -1, -1]])
         new_img = cv2.filter2D(img, -1, kernel)
         cv2.imwrite(output_directory + '/sharpen_' + file_name, new_img)
-
 
         # Повороты и отзеркаливания
         new_img = cv2.rotate(img, cv2.ROTATE_90_COUNTERCLOCKWISE)
@@ -43,6 +41,7 @@ def augmentation(paths: List[str], output_directory: str) -> None:
         new_img = cv2.rotate(img, cv2.ROTATE_90_CLOCKWISE)
         cv2.imwrite(output_directory + '/rotated_clockwise_' + file_name, new_img)
 
+        # mirror
         cv2.imwrite(output_directory + '/mirrored_' + file_name, np.flip(img, 1))
 
         # Блюр
