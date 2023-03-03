c0ff848445a91ff91e02e0418ab6d56ddd75222f
Nikolay Nyunin <nikolay.nyunin@gmail.com>
2023-03-03 17:39:08 +0300

    Implement rendering of camera position in Module_1b

--- Module_1b/camera_motion.py
+++ Module_1b/camera_motion.py
@@ -2,6 +2,7 @@ import os
 
 import cv2
 import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
 import numpy as np
 
 # path to the data used for calibration
@@ -57,37 +58,77 @@ for img in images:
 # camera calibration
 ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(obj_points, img_points, images[0].shape[:2], None, None)
 
+# number of previous camera positions to plot
+PATH_LEN = 50
+
 # list for all camera positions
-cam_positions = []
+cam_positions = [[], [], []]
 
-# TODO: do for all frames
-for img in images:
-    # converting the image to grayscale
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+# creating the graph for the camera position
+fig: plt.Figure = plt.figure(figsize=(4.0, 3.0))
+# fig.set_layout_engine('tight')
+ax: Axes3D = fig.add_subplot(projection='3d')
 
-    # finding the chessboard corners
-    # if desired number of corners are found in the image then ret == true
-    ret, corners = cv2.findChessboardCorners(gray, BOARD_DIM,
-                                             cv2.CALIB_CB_ADAPTIVE_THRESH + cv2.CALIB_CB_FAST_CHECK + cv2.CALIB_CB_NORMALIZE_IMAGE)
-    if ret:
-        # refining pixel coordinates for given 2d points
-        img_ps = cv2.cornerSubPix(gray, corners, (11, 11), (-1, -1), criteria)
+# prepare the data for rendering the chessboard
+x = np.arange(-100, 701, 100)
+y = np.arange(-100, 701, 100)
+X, Y = np.meshgrid(x, y)
+Z = np.zeros((9, 9))
 
-        ret, rvec, tvec = cv2.solvePnP(obj_ps, img_ps, mtx, dist, flags=cv2.SOLVEPNP_ITERATIVE)
+for path in os.listdir(DATA_DIR):
+    cap = cv2.VideoCapture(os.path.join(DATA_DIR, path))
+    while True:
+        frame_grabbed, img = cap.read()
+        if not frame_grabbed:
+            break
 
-        # rotation matrix
-        rot_mat = cv2.Rodrigues(rvec)[0]
+        # converting the image to grayscale
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-        # camera position
-        cam_pos = -np.matrix(rot_mat).T * np.matrix(tvec)
+        # finding the chessboard corners
+        # if desired number of corners are found in the image then ret == true
+        ret, corners = cv2.findChessboardCorners(gray, BOARD_DIM, cv2.CALIB_CB_ADAPTIVE_THRESH
+                                                 + cv2.CALIB_CB_FAST_CHECK + cv2.CALIB_CB_NORMALIZE_IMAGE)
+        if ret:
+            # refining pixel coordinates for given 2d points
+            img_ps = cv2.cornerSubPix(gray, corners, (11, 11), (-1, -1), criteria)
 
-        cam_positions.append(cam_pos)
+            # calculating the pose of the chessboard
+            _, rvec, tvec = cv2.solvePnP(obj_ps, img_ps, mtx, dist, flags=cv2.SOLVEPNP_ITERATIVE)
 
-cam_positions = np.array(cam_positions)
+            # rotation matrix
+            rot_mat = cv2.Rodrigues(rvec)[0]
 
-# plotting the camera's movement
-fig: plt.Figure = plt.figure()
-ax = fig.add_subplot(projection='3d')
-ax.plot(cam_positions[:, 0], cam_positions[:, 1], cam_positions[:, 2])
-ax.plot_surface(np.arange(-100, 701, 100), np.arange(-100, 701, 100), np.zeros((9, 9)))
-fig.show()
+            # camera position
+            cam_pos = -np.matrix(rot_mat).T * np.matrix(tvec)
+
+            # adding the current position to the path
+            for axis in range(3):
+                cam_positions[axis].append(cam_pos[axis, 0])
+
+            # deleting the oldest point if necessary
+            if len(cam_positions[0]) > PATH_LEN:
+                for axis in range(3):
+                    cam_positions[axis] = cam_positions[axis][1:]
+
+            # drawing the chessboard
+            ax.plot_surface(X, Y, Z, color='green')
+            # drawing the camera's path
+            ax.plot(cam_positions[0], cam_positions[1], cam_positions[2], color='blue', linewidth=2)
+            # drawing the current position of the camera
+            ax.scatter(cam_pos[0, 0], cam_pos[1, 0], cam_pos[2, 0], color='red', linewidths=3)
+
+            # making the graph have the same scale for all axes
+            ax.set_box_aspect([ub - lb for lb, ub in (getattr(ax, f'get_{a}lim')() for a in 'xyz')])
+
+            fig.canvas.draw()  # drawing the plot
+            w, h = fig.canvas.get_width_height()  # getting dimensions of the plot
+            # converting the graph image into a numpy array
+            plot_img = np.frombuffer(fig.canvas.tostring_rgb(), dtype=np.uint8).reshape((h, w, 3))
+            img[-plot_img.shape[0]:, -plot_img.shape[1]:, ::-1] = plot_img  # overlaying the graph on the frame
+            ax.clear()  # clearing the graph
+
+        cv2.imshow('camera motion', img)
+        cv2.waitKey(10)
+
+    cap.release()
