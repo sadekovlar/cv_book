77e5789c5781c347ce9b5ef8b54c56cb425db552
sergejlapin <sergey.l37@icloud.com>
2023-03-04 22:58:06 +0300

    Added task 2 Module 6

--- Module_6/task_2.py
+++ Module_6/task_2.py
@@ -1,89 +1,95 @@
-import cv2
 import numpy as np
-import os
+import cv2
+import time
+from cv_book.srccam.srccam.season_reader import SeasonReader
 
 
-class MotionDetection:
-    def __init__(self):
-        self.file_path = "../data/tram"
-        self.file_ext = "avi"
+class MovingObjects(SeasonReader):
+
+    def on_init(self):
+        self.lk_params = dict(winSize=(40, 40),
+                 maxLevel = 2,
+                 criteria = (cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_COUNT, 10, 0.03))
+        self.feature_params = dict(maxCorners =20,
+                      qualityLevel = 0.3,
+                      minDistance = 10,
+                      blockSize = 7)
+        self.trajectory_len = 20
+        self.detect_interval = 1
+        self.trajectories = []
+        self.frame_idx = 0
+        self.prev_gray = None
+        return True
+
+    def track_moving(self, frame):
+        start = time.time()
+        frame_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        img = frame.copy()
+        # prev_gray = frame_gray
+
+        if len(self.trajectories) > 0:
+            img0, img1 = self.prev_gray, frame_gray
+            p0 = np.float32([trajectory[-1] for trajectory in self.trajectories]).reshape(-1, 1, 2)
+            p1, _st, _err = cv2.calcOpticalFlowPyrLK(img0, img1, p0, None, **self.lk_params)
+            p0r, _st, _err = cv2.calcOpticalFlowPyrLK(img1, img0, p1, None, **self.lk_params)
+            d = abs(p0 - p0r).reshape(-1, 2).max(-1)
+            good = d < 1
+
+            new_trajectories = []
+            for trajectory, (x, y), good_flag in zip(self.trajectories, p1.reshape(-1, 2), good):
+                if not good_flag:
+                    continue
+                trajectory.append((x, y))
+                if len(trajectory) > self.trajectory_len:
+                    del trajectory[0]
+                new_trajectories.append(trajectory)
+                cv2.circle(img, (int(x), int(y)), 2, (0, 0, 255), -1)
+
+            self.trajectories = new_trajectories
+
+            cv2.polylines(img, [np.int32(trajectory) for trajectory in self.trajectories], False, (0, 255, 0))
+            cv2.putText(img, "track count : %d" % len(self.trajectories), (20, 50), cv2.FONT_HERSHEY_PLAIN, 1,
+                        (255, 25, 162), 4)
+
+        if self.frame_idx % self.detect_interval == 0:
+            mask = np.zeros_like(frame_gray)
+            mask[:] = 255
+
+            for x, y in [np.int32(trajectory[-1]) for trajectory in self.trajectories]:
+                cv2.circle(mask, (x, y), 5, 0, -1)
+
+            p = cv2.goodFeaturesToTrack(frame_gray, mask=mask, **self.feature_params)
+            if p is not None:
+                for x, y in np.float32(p).reshape(-1, 2):
+                    self.trajectories.append([(x, y)])
+
+        self.frame_idx += 1
+        self.prev_gray = frame_gray
+
+        end = time.time()
+        fps = 1 / (end - start)
+
+        cv2.putText(img, f"{fps:.2f} FPS", (20, 30), cv2.FONT_HERSHEY_PLAIN, 1, (255, 25, 162), 4)
+        cv2.imshow("Optical flow", img)
+        cv2.imshow("Mask", mask)
+
+    def on_shot(self):
+        return True
+
+    def on_frame(self):
+        self.track_moving(self.frame)
+        return True
+
+    def on_gps_frame(self) -> bool:
+        return True
 
 
-    def get_background(self, file_path):
-        cap = cv2.VideoCapture(file_path)
-        # we will randomly select 50 frames for the calculating the median
-        frame_indices = cap.get(cv2.CAP_PROP_FRAME_COUNT) * np.random.uniform(size=50)
-
-        # we will store the frames in array
-        frames = []
-        for idx in frame_indices:
-            # set the frame id to read that particular frame
-            cap.set(cv2.CAP_PROP_POS_FRAMES, idx)
-            ret, frame = cap.read()
-            frames.append(frame)
-
-        # calculate the median
-        median_frame = np.median(frames, axis=0).astype(np.uint8)
-
-        return median_frame
-
-    def detect(self, file_path):
-        cap = cv2.VideoCapture(file_path)
-        background = self.get_background(file_path)
-        # convert the background model to grayscale format
-        background = cv2.cvtColor(background, cv2.COLOR_BGR2GRAY)
-
-        frame_count = 0
-        consecutive_frame = 2
-
-        while (cap.isOpened()):
-            ret, frame = cap.read()
-            if ret == True:
-                frame_count += 1
-                orig_frame = frame.copy()
-                # IMPORTANT STEP: convert the frame to grayscale first
-                gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-                if frame_count % consecutive_frame == 0 or frame_count == 1:
-                    frame_diff_list = []
-                # find the difference between current frame and base frame
-                frame_diff = cv2.absdiff(gray, background)
-                # thresholding to convert the frame to binary
-                ret, thres = cv2.threshold(frame_diff, 50, 255, cv2.THRESH_BINARY)
-                # dilate the frame a bit to get some more white area...
-                # ... makes the detection of contours a bit easier
-                dilate_frame = cv2.dilate(thres, None, iterations=1)
-                # append the final result into the `frame_diff_list`
-                frame_diff_list.append(dilate_frame)
-                # if we have reached `consecutive_frame` number of frames
-                if len(frame_diff_list) == consecutive_frame:
-                    # add all the frames in the `frame_diff_list`
-                    sum_frames = sum(frame_diff_list)
-                    # find the contours around the white segmented areas
-                    contours, hierarchy = cv2.findContours(sum_frames, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-                    # draw the contours, not strictly necessary
-                    for i, cnt in enumerate(contours):
-                        cv2.drawContours(frame, contours, i, (0, 0, 255), 3)
-                    for contour in contours:
-                        # continue through the loop if contour area is less than 500...
-                        # ... helps in removing noise detection
-                        if cv2.contourArea(contour) < 500:
-                            continue
-                        # get the xmin, ymin, width, and height coordinates from the contours
-                        (x, y, w, h) = cv2.boundingRect(contour)
-                        # draw the bounding boxes
-                        cv2.rectangle(orig_frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
-
-                    cv2.imshow('Detected Objects', orig_frame)
-                    cv2.waitKey(1)
-                    if cv2.waitKey(100) & 0xFF == ord('q'):
-                        break
-            else:
-                break
-        cap.release()
-        cv2.destroyAllWindows()
-
-
-s = MotionDetection()
-data_files = sorted([f for f in os.listdir(s.file_path) if f.endswith(s.file_ext)])
-for file in data_files:
-    s.detect(os.path.join(s.file_path, file))
+if __name__ == "__main__":
+    for number in range(235, 236):
+        init_args = {
+            'path_to_data_root': '../data/tram/'
+        }
+        s = MovingObjects()
+        s.initialize(**init_args)
+        s.run()
+    print("Done!")
