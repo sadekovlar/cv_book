37f9aac7c003ada12a6ed96b33f9a2e6264b699f
Rinat <89263920392@mail.ru>
2023-03-05 23:47:33 +0300

    Добавил интрепретацию по преобразованию хафа

---
+++ Module_5/simple.py
@@ -0,0 +1,104 @@
+import cv2
+import numpy as np
+import math
+
+
+def hough_line(img, angle_step=1, lines_are_white=True, value_threshold=5):
+    """
+    Hough transform for lines
+    Input:
+    img - 2D binary image with nonzeros representing edges
+    angle_step - Spacing between angles to use every n-th angle
+                 between -90 and 90 degrees. Default step is 1.
+    lines_are_white - boolean indicating whether lines to be detected are white
+    value_threshold - Pixel values above or below the value_threshold are edges
+    Returns:
+    accumulator - 2D array of the hough transform accumulator
+    theta - array of angles used in computation, in radians.
+    rhos - array of rho values. Max size is 2 times the diagonal
+           distance of the input image.
+    """
+    # Rho and Theta ranges
+    thetas = np.deg2rad(np.arange(-90.0, 90.0, angle_step))
+    width, height = img.shape
+    diag_len = int(round(math.sqrt(width * width + height * height)))
+    rhos = np.linspace(-diag_len, diag_len, diag_len * 2)
+
+    # Cache some resuable values
+    cos_t = np.cos(thetas)
+    sin_t = np.sin(thetas)
+    num_thetas = len(thetas)
+
+    # Hough accumulator array of theta vs rho
+    accumulator = np.zeros((2 * diag_len, num_thetas), dtype=np.uint8)
+    # (row, col) indexes to edges
+    are_edges = img > value_threshold if lines_are_white else img < value_threshold
+    y_idxs, x_idxs = np.nonzero(are_edges)
+
+    # Vote in the hough accumulator
+    for i in range(len(x_idxs)):
+        x = x_idxs[i]
+        y = y_idxs[i]
+
+        for t_idx in range(num_thetas):
+            # Calculate rho. diag_len is added for a positive index
+            rho = diag_len + int(round(x * cos_t[t_idx] + y * sin_t[t_idx]))
+            accumulator[rho, t_idx] += 1
+
+    return accumulator, thetas, rhos
+
+def fast_hough_line(img, angle_step=1, lines_are_white=True, value_threshold=5):
+    """hough line using vectorized numpy operations,
+    may take more memory, but takes much less time"""
+    # Rho and Theta ranges
+    thetas = np.deg2rad(np.arange(-90.0, 90.0, angle_step)) #can be changed
+    #width, height = col.size  #if we use pillow
+    width, height = img.shape
+    diag_len = int(np.ceil(np.sqrt(width * width + height * height)))   # max_dist
+    rhos = np.linspace(-diag_len, diag_len, diag_len * 2)
+
+    # Cache some resuable values
+    cos_theta = np.cos(thetas)
+    sin_theta = np.sin(thetas)
+    num_thetas = len(thetas)
+
+    # Hough accumulator array of theta vs rho
+    accumulator = np.zeros((2 * diag_len, num_thetas))
+    are_edges = img > value_threshold if lines_are_white else img < value_threshold
+    #are_edges = cv2.Canny(img,50,150,apertureSize = 3)
+    y_idxs, x_idxs = np.nonzero(are_edges)  # (row, col) indexes to edges
+    # Vote in the hough accumulator
+    xcosthetas = np.dot(x_idxs.reshape((-1,1)), cos_theta.reshape((1,-1)))
+    ysinthetas = np.dot(y_idxs.reshape((-1,1)), sin_theta.reshape((1,-1)))
+    rhosmat = np.round(xcosthetas + ysinthetas) + diag_len
+    rhosmat = rhosmat.astype(np.int16)
+    for i in range(num_thetas):
+        rhos,counts = np.unique(rhosmat[:,i], return_counts=True)
+        accumulator[rhos,i] = counts
+    return accumulator, thetas, rhos
+
+def show_hough_line(img, accumulator, thetas, rhos):
+    import matplotlib.pyplot as plt
+
+    _, ax = plt.subplots(1, 2, figsize=(12, 12))
+
+    ax[0].imshow(img, cmap=plt.cm.gray)
+    ax[0].set_title('Входное изображение')
+    ax[0].axis('image')
+
+    ax[1].imshow(
+        accumulator, cmap='jet',
+        extent=[np.rad2deg(thetas[-1]), np.rad2deg(thetas[0]), rhos[-1], rhos[0]])
+    #ax[1].set_aspect('equal', adjustable='box')
+    ax[1].set_title('Преобразование Хафа')
+    ax[1].set_xlabel('Угол (градусы)')
+    ax[1].set_ylabel('Расстояние (пикселы)')
+    ax[1].axis('image')
+    plt.show()
+
+
+if __name__ == '__main__':
+    image = np.zeros([60,80])
+    frame = cv2.line(image, (20, 20), (50, 50), 255, 1)
+    accumulator, thetas, rhos = hough_line(frame)
+    show_hough_line(frame, accumulator, thetas, rhos)
